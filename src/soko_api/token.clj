2fe43dd064adb4c7fe4cf81aabc7da598bfe0de9
Scott Feeney <scott@oceanbase.org>
2014-01-24 03:24:50 -0500

    Qualify uses of jdbc functions
    This will avoid confusion as both jdbc and soko-api.token have somewhat
    similar sounding names (delete! vs. invalidate!, etc.)

--- src/soko_api/token.clj
+++ src/soko_api/token.clj
@@ -1,13 +1,13 @@
 (ns soko-api.token
   (:require soko-api.config
-            [clojure.java.jdbc :refer [query insert!]]
+            [clojure.java.jdbc :as jdbc]
             [clojure.string :as str]
             [crypto.random :as rand])
   (:refer soko-api.config :rename {dbspec db}))
 
 (defn lookup [token-str]
-  (first (query db ["SELECT email, token FROM tokens WHERE token = ?"
-                    token-str])))
+  (first (jdbc/query db ["SELECT email, token FROM tokens WHERE token = ?"
+                         token-str])))
 
 (defn- generate []
   (str/lower-case (rand/base32 15)))
@@ -15,5 +15,5 @@
 (defn create! [email]
   (let [record {:email email
                 :token (generate)}]
-    (insert! db :tokens record)
+    (jdbc/insert! db :tokens record)
     record))
