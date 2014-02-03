0540d645e0cd8f612053a35f4e17d59c245ab3c4
Scott Feeney <scott@oceanbase.org>
2014-02-03 14:59:17 -0600

    add-users-and-levels migration

--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -13,3 +13,25 @@
                   (text :email)
                   (text :token :primary-key))))
   (down [] (drop (table :tokens))))
+
+(defmigration add-users-and-levels
+  (up []
+      (create
+        (table :users
+               (text :email :unique)
+               (integer :id :primary-key :auto-inc)))
+      (create
+        (table :levels
+               (integer :id :primary-key :auto-inc)
+               (text :name :unique)))
+      (create
+        (table :user_levels
+               (integer :id :primary-key :auto-inc)
+               (integer :score)
+               (text :level)
+               (refer-to :users)
+               (refer-to :levels))))
+  (down []
+        (drop (table :user_levels))
+        (drop (table :users))
+        (drop (table :levels))))
