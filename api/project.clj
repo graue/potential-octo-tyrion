f622801a102765cef4b898a360a746b0c3d888b8
Scott Feeney <scott@oceanbase.org>
2014-02-16 10:16:00 -0500

    Update API deps

--- api/project.clj
+++ api/project.clj
@@ -7,13 +7,13 @@
                  [ring/ring-jetty-adapter "1.2.1"]
                  [ring/ring-codec "1.0.0"]
                  [ring/ring-json "0.2.0"]
-                 [clj-http "0.7.8"]
+                 [clj-http "0.7.9"]
                  [org.postgresql/postgresql "9.3-1100-jdbc41"]
                  [org.clojure/java.jdbc "0.3.0-beta1"]
                  [lobos "1.0.0-beta1"]
                  [environ "0.4.0"]
-                 [crypto-random "1.1.0"]]
-  :plugins [[lein-ring "0.8.8"]
+                 [crypto-random "1.2.0"]]
+  :plugins [[lein-ring "0.8.10"]
             [lein-environ "0.4.0"]]
   :ring {:handler soko-api.core/handler}
   :profiles
